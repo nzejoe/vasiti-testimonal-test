39e6fa5ba9ec37795319c54c074d159a9801ee17
Jonathan Nzete <jonathannzete@gmail.com>
2022-03-19 10:41:57 +0100

    hero-image styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { CustomNavbar, CustomLinks } from "./components";
+import { CustomNavbar, CustomLinks, Header } from "./components";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <CustomNavbar/>
-      <CustomLinks/>
+      <CustomNavbar />
+      <div className="main-content">
+        <CustomLinks />
+        <Header />
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Image } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <Container>
-      <div className="hero">
-        <div className="hero-info">
-          <h1 className="hero-header">
-            Amazing Experiences from Our Wonderful Customers
+    <Container className="hero">
+      <div className="hero-content d-md-flex">
+        <div className="hero-info ">
+          <h1 className="hero-title">
+            Amazing
+            <br />
+            Experiences from Our Wonderful Customers
           </h1>
-          <p>
+          <p className="hero-text">
             Here is what customers and vendors are saying about us, you can
             share your stories with us too.
           </p>
+        </div>
+        <div className="hero-image position-relative">
+          <div className="eclipse position-absolute"></div>
+          <div className="image-container position-absolute">
+            <Image src="/images/Testimonial image 1.png" />
+          </div>
         </div>
       </div>
     </Container>
