63c0ca435969a6aa34d5f297a00c67fd3db2e5e0
Jonathan Nzete <jonathannzete@gmail.com>
2022-03-19 21:46:09 +0100

    custom-links border

--- src/components/CustomLinks.jsx
+++ src/components/CustomLinks.jsx
@@ -3,28 +3,30 @@ import { Container, Nav} from 'react-bootstrap'
 
 const CustomLinks = () => {
   return (
-    <Container expand="md" className="custom-links d-none d-lg-block">
-      <Nav className="justify-content-around">
-        <Nav.Item>
-          <Nav.Link>market place</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>wholesale center</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>seller center</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>services</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>internship</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>events</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Container>
+    <div className="custom-links">
+      <Container expand="md" className="custom-links-inner d-none d-lg-block">
+        <Nav className="justify-content-around">
+          <Nav.Item>
+            <Nav.Link>market place</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>wholesale center</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>seller center</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>services</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>internship</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>events</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </div>
   );
 }
 
