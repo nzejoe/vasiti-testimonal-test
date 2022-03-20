a046e8ad8416d3e98c7a01acebf4328bbac55044
Jonathan Nzete <jonathannzete@gmail.com>
2022-03-20 09:39:09 +0100

    custom radio button

--- src/components/ShareStory.jsx
+++ src/components/ShareStory.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Modal, Button, Form, Row, Col} from 'react-bootstrap'
+import {Modal, Button, Form, Row, Col } from 'react-bootstrap'
 
 const ShareStory = ({ isEditing, handleEditing }) => {
 
@@ -22,7 +22,7 @@ const ShareStory = ({ isEditing, handleEditing }) => {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Text className="d-block mb-2">
                 Upload your Picture
               </Form.Text>
@@ -34,33 +34,60 @@ const ShareStory = ({ isEditing, handleEditing }) => {
 
             <Row>
               <Col>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3">
                   <Form.Label>First Name</Form.Label>
                   <Form.Control type="text" />
                 </Form.Group>
               </Col>
               <Col>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3">
                   <Form.Label>Last Name</Form.Label>
                   <Form.Control type="text" />
                 </Form.Group>
               </Col>
             </Row>
 
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group className="mb-3">
+              <Form.Label>Share your story</Form.Label>
+              <textarea className="form-control" rows={3}></textarea>
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            <Form.Group
+              className="mb-3 d-flex justify-content-between"
+              controlId="formBasicCheckbox"
+            >
+              <Form.Label className="check-label">
+                What did you interact with Vasiti as?
+              </Form.Label>
+              <div className='d-flex'>
+                <Form.Check
+                  inline
+                  label="Customer"
+                  name="identifier"
+                  type="radio"
+                  id="customer"
+                />
+                <Form.Check
+                  inline
+                  label="Vendor"
+                  name="identifier"
+                  type="radio"
+                  id="vendor"
+                />
+              </div>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="location-label">
+                City or Higher Institution (for Students)
+              </Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group>
+              <Button className="btn-lg submit-btn" onClick={handleClose}>
+                Share your story!
+              </Button>
+            </Form.Group>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
