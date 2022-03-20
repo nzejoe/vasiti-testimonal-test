499e55075ee8e5ed739fc2937ccfb537e04d7c23
Jonathan Nzete <jonathannzete@gmail.com>
2022-03-20 15:11:52 +0100

    file upload bug fix

--- src/components/ShareStory.jsx
+++ src/components/ShareStory.jsx
@@ -1,16 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+import { MdOutlineAttachment, MdClear } from "react-icons/md";
 
 const ShareStory = ({ isEditing, handleEditing }) => {
   const [selectedIdentifier, setSelectedIdentifier] = useState("");
+  const [image, setImage] = useState('');
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const textRef = useRef(null);
+  const locationRef = useRef(null);
+  const imageRef = useRef(null);
+
+
 
   const handleClose = () => {
     handleEditing(false);
+    handleSubmit();
   };
 
   const handleIdentifier = (e) => {
     const selected = e.target;
     if (selected.checked) setSelectedIdentifier(selected.id);
+  };
+
+  const handleFileChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleClearImage = (e) => {
+    setImage('');
+    if(imageRef.current) imageRef.current.value = null;
+  };
+
+  const handleSubmit = () => {
+    const firstName = firstNameRef.current && firstNameRef.current.value;
+    const lastName = lastNameRef.current && lastNameRef.current.value;
+    const location = locationRef.current && locationRef.current.value;
+    const text = textRef.current && textRef.current.value;
+    const name = `${firstName} ${lastName}`;
+    const data = {
+      badge: selectedIdentifier,
+      image,
+      name,
+      text,
+      location,
+    };
+    console.log(data);
   };
 
   return (
@@ -31,30 +66,43 @@ const ShareStory = ({ isEditing, handleEditing }) => {
               <Form.Text className="d-block mb-2">
                 Upload your Picture
               </Form.Text>
-              <Form.Label className="custom-input d-block w-100">
-                Choose Image
+              <span className="icon-container float-end mt-2 me-3">
+                {image ? (
+                  <MdClear onClick={handleClearImage} />
+                ) : (
+                  <MdOutlineAttachment />
+                )}
+              </span>
+              <Form.Label className="custom-input d-block w-100 ">
+                <div className="file-label">
+                  {image ? image.name : "Choose Image"}
+                </div>
               </Form.Label>
-              <Form.Control type="file" placeholder="Enter email" hidden />
+              <Form.Control type="file" ref={imageRef} hidden onChange={handleFileChange} />
             </Form.Group>
 
             <Row>
               <Col>
                 <Form.Group className="mb-3">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" ref={firstNameRef} />
                 </Form.Group>
               </Col>
               <Col>
                 <Form.Group className="mb-3">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" ref={lastNameRef} />
                 </Form.Group>
               </Col>
             </Row>
 
             <Form.Group className="mb-3">
               <Form.Label>Share your story</Form.Label>
-              <textarea className="form-control" rows={3}></textarea>
+              <textarea
+                className="form-control"
+                rows={3}
+                ref={textRef}
+              ></textarea>
             </Form.Group>
             <Form.Group
               className="mb-3 d-flex justify-content-between"
@@ -86,10 +134,11 @@ const ShareStory = ({ isEditing, handleEditing }) => {
               <Form.Label className="location-label">
                 City or Higher Institution (for Students)
               </Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" ref={locationRef} />
             </Form.Group>
             <Form.Group>
               <Button
+                type="button"
                 className="btn-lg float-end submit-btn"
                 onClick={handleClose}
               >
